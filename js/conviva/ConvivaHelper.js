65a2a9d5af4f3b99661e919fab0edb79d851f11b
d353163 <damitha.n.gunawardena@team.telstra.com>
2019-12-05 16:51:12 +1100

    first commit

--- js/conviva/ConvivaHelper.js
+++ js/conviva/ConvivaHelper.js
@@ -261,7 +261,7 @@ var ConvivaHelper = function (options) {
     // A human-readable identifier for your application.
     // Very helpful to filter traffic and compare performance for different builds of
     // the video application.
-    contentMetadata.applicationName = 'BrightCoveHTML5Playar';
+    contentMetadata.applicationName = 'BrightCoveHTML5Player';
 
     // An identifier for the current user. Can be obfuscated to ensure privacy.
     // Can be used to isolate video traffic for a particular and help with
