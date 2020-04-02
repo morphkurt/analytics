65966e7a308b109fc0c96191811577ba9131a972
d353163 <damitha.n.gunawardena@team.telstra.com>
2020-04-02 13:27:22 +1100

    add

--- js/videojs.telstra-360.js
+++ js/videojs.telstra-360.js
@@ -1,7 +1,5 @@
 videojs.registerPlugin('telstra-360', function (options) {
     __indexOf = [].indexOf || function (item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
     var player = this
-    if ( player.mediainfo.customFields["content_provider"] == "360" ){
-        player.vr({projection: '360'});
-    }
+    player.vr({projection: 'AUTO'});
 });
